c926fa2d2dd16734e1e59cc803a4812b0f12fcee
warlock <fecypher@gmail.com>
2024-06-21 18:00:52 +0100

    copyright page

---
+++ app/Copyright/page.tsx
@@ -0,0 +1,54 @@
+import Footer from "@/components/home/Footer";
+import NavigationBar from "@/components/home/Nabar";
+import { BottomHeader } from "@/components/shared/BottomHeader";
+import PageHeader from "@/components/shared/PageHeader";
+import React from "react";
+
+const CopyrightPage = () => {
+  return (
+    <>
+      <NavigationBar />
+      <PageHeader title="Copyright" />
+      <div className="lg:w-[50%] mx-auto p-4">
+        {/* our place in the industry */}
+        <div>
+          <p className="text-sm leading-loose my-3">
+            Unless otherwise noted, all content on this website, including, but
+            not limited to, text, graphics, audio clips, videos, television
+            clips, logos, buttons, images, digital downloads, data compilations,
+            analysis, icons, html code and xml code, is the property of R.J.
+            O’Brien & Associates, LLC, its content vendors, or other third
+            parties and is protected by U.S. and international copyright laws.
+          </p>
+          <p className="text-sm leading-loose my-3">
+            The compilation of all content on this website is the exclusive
+            property of R.J. O’Brien & Associates, LLC or its content vendors
+            and is protected by U.S. and international copyright laws.
+          </p>
+          <p className="text-sm leading-loose my-3">
+            All software used on this website is the exclusive property of R.J.
+            O’Brien & Associates, LLC or its software suppliers and is protected
+            by U.S. and international copyright laws as well as other laws.
+          </p>
+          <p className="text-sm leading-loose my-3">
+            If you believe any material available through this website infringes
+            a copyright, you should notify R.J. O’Brien & Associates, LLC as
+            described below:
+          </p>
+          <div>
+            <p className="font-bold text-sm my-2">Address:</p>
+            <div className="text-sm space-y-3">
+              <p className="font-bold">Legal Department</p>
+              <p>R.J. O’Brien & Associates, LLC</p>
+              <p className="text-green-500">+1 312-682-2127</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <BottomHeader title="Connect with RJO today!" />
+      <Footer />
+    </>
+  );
+};
+
+export default CopyrightPage;
---
+++ app/clearing-closure/page.tsx
@@ -0,0 +1,38 @@
+import Footer from "@/components/home/Footer";
+import NavigationBar from "@/components/home/Nabar";
+import { BottomHeader } from "@/components/shared/BottomHeader";
+import PageHeader from "@/components/shared/PageHeader";
+import React from "react";
+
+const ClearingClosure = () => {
+  return (
+    <>
+      <NavigationBar />
+      <PageHeader title="Clearing Criteria Disclosures" />
+      <div className="lg:w-[50%] mx-auto p-4">
+        {/* our place in the industry */}
+        <div>
+          <p className="text-sm leading-loose my-3">
+            R.J. O’Brien provides clearing services to various types of
+            customers including corporations, agricultural firms and other
+            hedgers, hedge funds and institutional asset managers and
+            individuals.
+          </p>
+          <p className="text-sm leading-loose my-3">
+            In determining whether to offer clearing services to prospective
+            customers, R.J. O’Brien considers objective criteria including,
+            without limitation, the customer’s investment profile, financial
+            health, legal character, industry reputation and the relevant legal
+            and commercial terms and, where required, the customer’s status as
+            an Eligible Contract Participant (as such term is defined in the
+            Commodity Exchange Act).
+          </p>
+        </div>
+      </div>
+      <BottomHeader title="Connect with RJO today!" />
+      <Footer />
+    </>
+  );
+};
+
+export default ClearingClosure;
