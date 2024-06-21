525da72d50d9b7bf9c5ddd6ce79351e155b30407
warlock <fecypher@gmail.com>
2024-06-22 00:05:03 +0100

    testing2

--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-export async function GET(req: Request, res: NextApiResponse) {
+export async function GET(res: NextApiResponse) {
   let uri: any = process.env.WEBHOOK_SECRET;
 
-  res.status(200).json({
-    uri,
-  });
+  return res.status(200).json({
+   uri,
+ });
 }
