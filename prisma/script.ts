a1a8e007bb8621b8692f3120ee634603cc482672
warlock <fecypher@gmail.com>
2024-06-22 14:00:20 +0100

    test hook 2

--- prisma/script.ts
+++ prisma/script.ts
@@ -1,6 +1,18 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+let prisma: PrismaClient;
 
+declare global {
+  var prisma: PrismaClient | undefined;
+}
 
-// changed something
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export { prisma };
