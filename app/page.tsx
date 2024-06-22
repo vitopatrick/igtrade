ef54b18795b47ee0f95f8f105083e2994ba5dcb7
warlock <fecypher@gmail.com>
2024-06-22 01:26:19 +0100

    addediwe

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Footer from "@/components/home/Footer";
 import Hero from "@/components/home/Hero";
 import NavigationBar from "@/components/home/Nabar";
 import Services from "@/components/home/Services";
-import { connectDb } from "@/lib/db";
 
 const Home = async () => {
   return (
