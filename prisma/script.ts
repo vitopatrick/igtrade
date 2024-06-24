0d597582ed4328b75aa170783bb967986c4fbc88
warlock <fecypher@gmail.com>
2024-06-24 17:28:09 +0100

    changed files

--- prisma/script.ts
+++ prisma/script.ts
@@ -1,18 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
-let prisma: PrismaClient;
+let prisma = new PrismaClient();
 
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
 
 export { prisma };
